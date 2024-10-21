9be81b6b183e56f270f6cbc84d507ba7dd4dcb08
mushroom223 <sasalkinesketit@gmail.com>
2024-10-22 02:27:39 +0300

    Start
    So i added 2 items and item group.

---
+++ src/client/java/github/floatingheaven33/GymModClient.java
@@ -0,0 +1,10 @@
+package github.floatingheaven33;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GymModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/github/floatingheaven33/GymModDataGenerator.java
@@ -0,0 +1,11 @@
+package github.floatingheaven33;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GymModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
